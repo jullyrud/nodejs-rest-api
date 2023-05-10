3cb2211fb1f41e5bf8ad01f28ed3bb91f3d76420
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-05-10 23:39:58 +0200

    add cinfig.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const app = require('./app')
-
+const {DB_HOST} = require('./config')
 const mongoose = require('mongoose')
-
-const DB_HOST = 'mongodb+srv://jullyrud:Sosison23sm@cluster0.pnxjq0y.mongodb.net/contacts_reader?retryWrites=true&w=majority'
 
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000")
+    console.log("Database connection successful")
 })
   })
   .catch((err) => {
     console.log(err.message)
     process.exit(1)
   })
+
+  console.log(process.env);
