5ede4f20d7f58b840b909b3f27d4d7f31675eea0
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-05-10 22:50:14 +0200

    mongoose.connect

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require('mongoose')
+
+const DB_HOST = 'mongodb+srv://jullyrud:Sosison23sm@cluster0.pnxjq0y.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
 })
+  })
+  .catch((err) => {
+    console.log(err.message)
+    process.exit(1)
+  })
