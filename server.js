7dba16cbd7282fc9c034a8da6724c42050d23093
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-05-11 10:52:15 +0200

    add strictQuery

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require('./app')
 const {DB_HOST} = process.env
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', true)
+
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
